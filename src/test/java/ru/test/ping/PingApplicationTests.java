a8c4398b6c1f4b5f7178efe1bcbee70505cb6fae
Oleg G <oleg.golubovsky17041986@gmail.com>
2024-05-28 07:05:11 +0300

    Init project.

---
+++ src/test/java/ru/test/ping/PingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.ping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
